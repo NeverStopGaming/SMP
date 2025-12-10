29a69e2427b5d94a7d2877c7209cc0f49b14fa46
BenDerFarmer <chaoten@neverstopgaming.net>
2025-12-10 21:41:26 +0100

    feat: Add FabricMessage to open questBook and request mods

---
+++ modules/QuestSystem/src/main/kotlin/net/derfarmer/questsystem/command/ModsCommand.kt
@@ -0,0 +1,29 @@
+package net.derfarmer.questsystem.command
+
+import net.derfarmer.moduleloader.commands.Command
+import net.derfarmer.moduleloader.commands.annotations.CommandArgument
+import net.derfarmer.moduleloader.commands.annotations.CommandSubPath
+import net.derfarmer.moduleloader.commands.provider.PlayerCommandSuggestionProvider
+import net.derfarmer.moduleloader.sendMSG
+import net.derfarmer.questsystem.FabricManager
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+
+object ModsCommand : Command("mods") {
+
+    @CommandSubPath("<targetName>", "fabric.mods")
+    fun handle(player: Player, @CommandArgument("targetName", PlayerCommandSuggestionProvider::class) targetName : String) {
+
+        val target = Bukkit.getPlayer(targetName)
+
+        if (target == null) {
+            player.sendMSG("player.notfound")
+            return
+        }
+
+        FabricManager.requestModes(player) { data ->
+            val msg = data.split(";").joinToString { it.split("%%%")[0] }
+            player.sendMSG("mods.player", target.name, msg)
+        }
+    }
+}
